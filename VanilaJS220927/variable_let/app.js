542c3367172d87caa19082c0a9578a3db4b813c1
d-minn <d-minn>
2022-09-27 14:46:57 +0900

    회사에서JS커밋/남은건 집가서고

---
+++ VanilaJS220927/variable_let/app.js
@@ -0,0 +1,20 @@
+alert("hi");
+
+let a = 5;
+let b = 2;
+let myName = "dm";
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello"+myName);
+
+/*
+const와 let의 차이
+const는 constant(상수)라는 것이고, 값이 바뀔 수 없다.
+let은 새로운 것을 생성할 때 사용하며 한번 생기고 나서 값을 업데이트하고 싶을 때 업데이트할 수 있다.
++업데이트할 때는 let OOO가 아니라 그냥 OOO = ?라고 선언하면 업데이트가 됨.
+
+*/
+
